e53a6e46add1d6711e172fac6a37cc8145ae40d4
maheshmiythran <maheshmiythran.bk@gmail.com>
2025-07-21 23:40:22 +0530

    sdsad

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -43,7 +43,7 @@ const AddAddress = () => {
     const onSubmitHandler = async (e) => {
         e.preventDefault();
         try {
-            const {data} = await axios.post('/api/address/add', { address, userId: user._id });
+            const {data} = await axios.post('/api/address/add', { ...address, userId: user._id });
             if (data.success) {
                 toast.success(data.message);
                 navigate('/cart');
